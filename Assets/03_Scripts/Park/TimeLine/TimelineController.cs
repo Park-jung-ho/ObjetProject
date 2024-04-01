fa91aa95444abdd6e53373551387478e1ce8054d
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2024-04-02 03:01:23 +0900

    ✨다이얼로그 에셋 추가
    다이얼로그 에셋과 기존 스크립트 연동중

--- Assets/03_Scripts/Park/TimeLine/TimelineController.cs
+++ Assets/03_Scripts/Park/TimeLine/TimelineController.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using PixelCrushers.DialogueSystem;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class TimelineController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public PlayableDirector playableDirector;
+    public TimelineAsset[] timelines; 
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
+    public void playCutscene(int id)
+    {
+        playableDirector.playableAsset = timelines[id];
+        playableDirector.Play();
+    }
+    public void playCutsceneAuto()
+    {
+        int id = DialogueLua.GetVariable("timeline").AsInt;
+        if (id == -1) return;
+        playableDirector.playableAsset = timelines[id];
+        playableDirector.Play();
+    }
+    
 }
