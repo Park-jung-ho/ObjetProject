7f0b0f55736587ddc9582fa357eb8a477940ee30
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2024-04-16 02:22:19 +0900

    글리치 카메라쉐이크 이벤트 추가

--- Assets/03_Scripts/Park/GlitchController.cs
+++ Assets/03_Scripts/Park/GlitchController.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GlitchController : MonoBehaviour
 {
+    public UnityEvent Onshake;
     public Material mat;
     public float Vignette;
     // Update is called once per frame
     void Update()
     {
         mat.SetFloat("_Vignette",Vignette);
+    }
+
+    public void animEvent()
+    {
+        Onshake.Invoke();
     }
 }
