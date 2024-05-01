b7c15e8b0e047ce4864b1583655c73036408a9c7
Da Bin Son <dabini906@naver.com>
2024-05-01 19:05:34 +0900

    npc움직임

---
+++ Assets/03_Scripts/Son/NPC/NpcMove.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcMove : MonoBehaviour
+{
+    public enum MoveType
+    {
+        Move,
+        Look
+    }
+    public float moveSpeed = 3.0f; // 이동 속도
+    public Transform moveTarget;
+    MoveType moveType;
+    GameObject player;
+    float distanceToTarget;
+    void Start()
+    {
+        moveType = MoveType.Look;
+        player = GameObject.Find("Player");
+        distanceToTarget = Vector3.Distance(transform.position, player.transform.position);
+    }
+
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(transform.position, player.transform.position);
+        if (moveType == MoveType.Move)
+        {
+            Debug.Log("Move");
+            Vector3 direction = (moveTarget.transform.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+            // target의 위치로 부드럽게 이동
+            float step = moveSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, moveTarget.position, step);
+            if (distanceToTarget > 8f) moveType = MoveType.Look;
+        }
+
+        if (moveType == MoveType.Look)
+        {
+            Debug.Log("Look");
+            Vector3 direction = (player.transform.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+            if (distanceToTarget < 8f) moveType = MoveType.Move;
+        }
+    }
+}
