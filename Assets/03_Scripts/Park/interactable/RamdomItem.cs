c31d7f34c885fe9968250e00954002cb31ab75af
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2024-05-05 00:40:54 +0900

    ✨ 랜덤아이템 추가

---
+++ Assets/03_Scripts/Park/interactable/RamdomItem.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RamdomItem : MonoBehaviour, interactable2D
+{
+    public interactType type {get; set;}
+    public Item item;
+    [SerializeField]
+    private bool isInteractable = false;
+
+    void Start()
+    {
+        type = interactType.Object;
+    }
+
+    void Update()
+    {
+        
+    }
+    public void trigger(bool can)
+    {
+        isInteractable = can;
+    }
+    public bool CanClick()
+    {
+        return isInteractable;
+    }
+    public void Interact()
+    {
+        bool successToInven = InventoryManager.instance.AddItem(item);
+        if (successToInven) GoToInven();
+    }
+
+    public void GoToInven()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
