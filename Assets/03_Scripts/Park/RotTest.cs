9d1ef442fc0f21acb4f32b4334e1bf2e6e98978d
Park-jung-ho <32171761@dankook.ac.kr>
2024-06-17 19:30:27 +0900

    3D오브젝트 마우스로 회전 테스트

---
+++ Assets/03_Scripts/Park/RotTest.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotTest : MonoBehaviour
+{
+    public float rotspeed;
+    private bool dragging = false;
+    private Rigidbody _rigidbody;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>(); 
+    }
+
+    void OnMouseDrag() 
+    {
+        dragging = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            dragging = false;
+        }
+    }
+    void FixedUpdate() 
+    {
+        if (dragging)
+        {
+            float x = Input.GetAxis("Mouse X") * rotspeed * Time.fixedDeltaTime;
+            float y = Input.GetAxis("Mouse Y") * rotspeed * Time.fixedDeltaTime;
+
+            _rigidbody.AddTorque(Vector3.down * x);
+            _rigidbody.AddTorque(Vector3.right * y);
+        }
+    }
+}
